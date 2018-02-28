21225e5c2bdd3780d5fc61392f3541f7e4adb57c
Durban Frazer <not_real_email@not_real_email.com>
2018-02-28 09:19:11 +0000

    solution to problem 0002

--- euler_project/functions/math.py
+++ euler_project/functions/math.py
@@ -1,3 +1,12 @@
 
 def summation(x):
     return x*(x+1)/2
+    
+
+''' get the xth fibonacci number'''
+_fibonacci_map = {1:1,2:2}
+def fibonacci(x):
+    if (x not in _fibonacci_map):
+        _fibonacci_map[x] = fibonacci(x-1) + fibonacci(x-2)
+    return _fibonacci_map[x]
+    
